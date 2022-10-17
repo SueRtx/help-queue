9d4c5589fef33c021231bdac8c578125d5f9f90b
Sue Roberts <suerobertstx@yahoo.com>
2022-10-16 23:04:55 -0500

    React class work week 3

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import TicketControl from "./TicketControl";
+
+function App(){
+  return ( 
+    <React.Fragment>
+      <Header />
+      <TicketControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
+
+{/* This is a JSX comment. */}
